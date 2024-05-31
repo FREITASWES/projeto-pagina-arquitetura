c83303e74174258e1d75cc05b9a6ad32fce9ef85
freitass.wesley <freitass.wesley@gmail.com>
2024-05-31 11:43:52 -0300

    Versao Inicial

---
+++ pasta JS/index.js
@@ -0,0 +1,12 @@
+var botao = window.document.getElementById("botao")
+var imagem = window.document.getElementById("imagem")
+
+
+function EsconderBotao() {
+    botao.style = "display:none"
+}
+
+
+function EsconderImagem() {
+    imagem.style = "display:none"
+}
